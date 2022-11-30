f4cc9c8fb7902cd34ce5dff00274e9b6d124b5f3
Misha Causur <mishacausur@mail.ru>
2022-11-30 12:38:47 +0500

    small fixes

--- Converter/Services/DataManager/DataManager.swift
+++ Converter/Services/DataManager/DataManager.swift
@@ -23,27 +23,24 @@ final class DataManager {
     }
     
     var dataToConvert: (to: String, from: String, amount: Int) {
-        let from = lastChangedField == .upper ? firstCurrencySign : secondCurrencySign
-        let to = lastChangedField == .upper ? secondCurrencySign : firstCurrencySign
+        let from = lastChangedField == .upper ? _firstCurrency.value.sign : _secondCurrency.value.sign
+        let to = lastChangedField == .upper ? _secondCurrency.value.sign : _firstCurrency.value.sign
         let amount = lastChangedField == .upper ? firstCurrencyValue : secondCurrencyValue
         return (to, from, amount)
     }
     
-    private let _firstCurrency = PublishRelay<Currency>()
-    private let _secondCurrency = PublishRelay<Currency>()
+    /// Private properties
+    private let _firstCurrency = BehaviorRelay<Currency>(value: .init(sign: "", name: ""))
+    private let _secondCurrency = BehaviorRelay<Currency>(value: .init(sign: "", name: ""))
     private let _valueEntered = BehaviorRelay(value: false)
     private(set) var firstCurrencyValue: Int = 0
     private(set) var secondCurrencyValue: Int = 0
-    private var firstCurrencySign = ""
-    private var secondCurrencySign = ""
    
     func storeCurrency(_ currency: Currency, into: CurrencyButton) {
         switch into {
         case .upper:
-            firstCurrencySign = currency.sign
             _firstCurrency.accept(currency)
         case .lower:
-            secondCurrencySign = currency.sign
             _secondCurrency.accept(currency)
         }
     }
@@ -59,6 +56,7 @@ final class DataManager {
         }
         checkValues()
     }
+    
     /// Checking properties
     private (set) var lastChangedField: CurrencyButton = .upper
 
